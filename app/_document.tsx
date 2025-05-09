f6bfccc8ca4efb16c815e6a3757425e03e7edbde
usernotDeadMysterious <you@example.com>
2025-05-09 16:21:04 +0500

    Seo process

---
+++ app/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Add the canonical link tag */}
+          <link rel="canonical" href="https://aizaz-khalid-portfolio-website.vercel.app/" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://aizaz-khalid-portfolio-website.vercel.app/', 
+    generateRobotsTxt: true,
+  };
+  
