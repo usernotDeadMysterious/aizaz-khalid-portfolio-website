302d41641e3de762bb4f93e1698905084ded3d35
usernotDeadMysterious <you@example.com>
2025-06-12 13:59:32 +0500

    adding Google Analytics

--- app/_document.tsx
+++ app/_document.tsx
@@ -7,6 +7,24 @@ class MyDocument extends Document {
         <Head>
           {/* Add the canonical link tag */}
           <link rel="canonical" href="https://aizaz-khalid-portfolio-website.vercel.app/" />
+
+           {/* Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-6BXRP55Z4E"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-6BXRP55Z4E');
+              `,
+            }}
+          />
+
+
         </Head>
         <body>
           <Main />
