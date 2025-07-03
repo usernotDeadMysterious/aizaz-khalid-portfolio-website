91cabb1b30530724e1dab71d53d6cb5067569e6d
usernotDeadMysterious <you@example.com>
2025-07-03 23:54:24 +0500

    vercel error fixing

--- app/(auth)/_components/login.tsx
+++ app/(auth)/_components/login.tsx
@@ -1,5 +1,6 @@
+'use client'
 import { FormEvent } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter} from 'next/navigation'
  
 export default function LoginPage() {
   const router = useRouter()
